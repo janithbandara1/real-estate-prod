03a80bb376127da92b7001ffd492c1dd82ce7944
janithbandara2 <janithbandara002@gmail.com>
2025-05-19 12:22:29 +0000

    images final adjustment

--- client/next.config.ts
+++ client/next.config.ts
@@ -9,17 +9,13 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "*.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
-  },
-  experimental: {
-    serverActions: {
-      allowedOrigins: [
-        "localhost:3000", // localhost
-        "localhost:3001",
-        "glorious-palm-tree-695pj947q57gcrpx7-3000.app.github.dev", // Codespaces
-        "glorious-palm-tree-695pj947q57gcrpx7-3001.app.github.dev",
-      ],
-    },
   },
 };
 
